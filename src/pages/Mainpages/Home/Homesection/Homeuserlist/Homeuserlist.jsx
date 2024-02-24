c864aeee5a21b56ac6353ef2f92bea27c6082af2
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2024-02-24 10:35:26 +0600

    validation_update_mainpages_home_homesection_homeuserlist_part_001

--- src/pages/Mainpages/Home/Homesection/Homeuserlist/Homeuserlist.jsx
+++ src/pages/Mainpages/Home/Homesection/Homeuserlist/Homeuserlist.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Homeuserlist.css'
 import Grouphead from '../../../../../Component/Groupshead/Grouphead'
-import { FaPlus } from "react-icons/fa6";
-import { FaMinus } from "react-icons/fa6";
+import { getDatabase, ref, onValue , set , push ,remove } from "firebase/database";
+import { useSelector, useDispatch } from 'react-redux'
 
 const Homeuserlist = () => {
+    const db = getDatabase();
+    const data = useSelector((state) => state.alldata.value)
+
+    let [userList , setUserList] = useState([])
+
+    useEffect (()=>{
+        const userListRef = ref(db, 'usersdata');
+            onValue(userListRef, (snapshot) => {
+                let array = []
+                snapshot.forEach((item)=>{
+                   if(data.uid != item.key){
+                        array.push({...item.val(),id:item.key})
+                   }
+                })
+                setUserList(array)
+        });
+    },[])
+    console.log(data);
+    console.log(userList);
+    
   return (
    <section id='homeuserlist'>
         <div className='homeuserlist_wrapper'>
@@ -12,20 +32,20 @@ const Homeuserlist = () => {
                 <Grouphead grouphead="User List"/>
                 <div className='homeuserlist_wrapper_box_flex'>
                     {
-                        [ 1 , 2 , 3 , 4 ,5 , 6 , 7 , 8 ].map((item , index)=>(
+                        userList.map((item , index)=>(
                             <div key={index} className='homeuserlist_profile_wrapper'>
                                 <div className='homeuserlist_profile_box'>
                                     <div className='homeuserlist_list_profile_image'>
-                                        <img src="" alt="" />
+                                        <img src={item.profileImage} alt="not found" />
                                     </div>
                                     <div className='homeuserlist_profile_name'>
-                                        <h4 className='homeuserlist_profile_friend_name'>Raghav</h4>
+                                        <h4 className='homeuserlist_profile_friend_name'>{item.username}</h4>
                                         <h5 className='homeuserlist_profile_online'>56pm</h5>
                                     </div>
                                 </div>
                                 <div className='homeuserlist_profile_add_btn'>
                                         <button className='homeuserlist_profile_btn'>
-                                            <FaPlus className='homeuserlist_profile_add' />
+                                            add
                                         </button>
                                 </div>
                             </div>
