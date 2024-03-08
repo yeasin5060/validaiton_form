a12790bed94aaac2b2e49fa6762fd43d45f76b63
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2024-03-08 10:13:58 +0600

    update_message_005

--- src/pages/Mainpages/Message/Message.jsx
+++ src/pages/Mainpages/Message/Message.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import "./Message.css"
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <section id='message_full_body'>
+      <div className='container'>
+        <div className='message_child_flex'>
+          <div className='messaga_friend_list_box'>
+              <h3 className='message_friend_list'>Friend List</h3>
+          </div>
+          <div className='message_messageing_box'>
+              <div className='message_friend_name_box'>
+                <h3 className='message_friend_name'>yeasin</h3>
+                <p className='message_friend_active'>active now</p>
+              </div>
+              <div className='message_messageing_list_flex'>
+                
+              </div>
+              <div className='message_text_box_flex'>
+                  <input className='message_input_box' type="text" placeholder='inter your message' />
+                  <div className='message_uplode_btn_box'>
+                    <button className='message_uplode_btn'>send</button>
+                  </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
