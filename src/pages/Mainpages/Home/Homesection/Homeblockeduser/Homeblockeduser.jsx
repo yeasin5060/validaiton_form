3303d37a281f548efd2ec039de64eefb5940e279
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2024-03-13 11:15:29 +0600

    validation_update_home_homeblockeduser_read_data_in_blockedusers_database

--- src/pages/Mainpages/Home/Homesection/Homeblockeduser/Homeblockeduser.jsx
+++ src/pages/Mainpages/Home/Homesection/Homeblockeduser/Homeblockeduser.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Homeblockeduser.css'
 import Grouphead from '../../../../../Component/Groupshead/Grouphead'
+import { getDatabase, ref, onValue , set , push ,remove } from "firebase/database";
+import { useSelector, useDispatch } from 'react-redux'
+import { useEffect } from 'react';
 
 const Homeblockeduser = () => {
+    const db = getDatabase();
+    const data = useSelector((state) => state.alldata.value)
+
+    let [blockList , setBlockList] = useState()
+
+    useEffect(()=>{
+       const blockListRef = ref(db, 'blockedusers');
+           onValue(blockListRef, (snapshot) => {
+              let array = []
+           snapshot.forEach((item)=>{
+               if(data.uid != item.val().whereofblockid){
+                   array.push({...item.val(),id:item.key})
+               }
+           })
+           setBlockList(array)
+       });
+   },[])
+
+   let handelunblockuser = (unblockinfo)=>{
+           remove(ref(db, 'blockedusers/' + unblockinfo.id))
+               alert("Unblock Succesful")
+   }
+   console.log(blockList);
   return (
     <section id='homeblockeduser'>
         <div className='homeblockeduser_wrapper'>
@@ -10,19 +36,19 @@ const Homeblockeduser = () => {
                 <Grouphead grouphead="Blocked Users"/>
                 <div className='homeblockeduser_wrapper_box_flex'>
                     {
-                        [ 1 , 2 , 3 , 4 ,5 , 6 , 7 , 8 ].map((item , index)=>(
+                        blockList &&
+                        blockList.map((item , index)=>(
                             <div key={index} className='homeblockeduser_profile_wrapper'>
                                 <div className='homeblockeduser_profile_box'>
                                     <div className='homeblockeduser_list_profile_image'>
-                                        <img src="" alt="" />
+                                        <img src={item. whereofblockimg} alt="not found" />
                                     </div>
                                     <div className='homeblockeduser_profile_name'>
-                                        <h4 className='homeblockeduser_profile_friend_name'>Raghav</h4>
-                                        <h5 className='homeblockeduser_profile_online'>Hosen</h5>
+                                        <h4 className='homeblockeduser_profile_friend_name'>{item.whereofblockname}</h4>
                                     </div>
                                 </div>
                                 <div className='homeblockeduser_profile_add_btn'>
-                                        <button className='homeblockeduser_profile_btn'>
+                                        <button onClick={()=> handelunblockuser(item)} className='homeblockeduser_profile_btn'>
                                            Unblock
                                         </button>
                                 </div>
